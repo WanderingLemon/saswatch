24b70280d4375b799001609f38de1b37311b32e8
WanderingLemon <wisdomcr9@gmail.com>
2025-10-30 23:31:18 -0400

    Broke the slider library into two modules

--- sliders/src/lib.rs
+++ sliders/src/lib.rs
@@ -1,13 +1,2 @@
-use std::ops::Range;
-
-#[derive(Default)]
-pub struct SliderState {
-    position: usize,
-    range: Range<usize>,
-}
-
-impl SliderState {
-    pub fn new(position: usize, range: Range<usize>) -> Self {
-        Self { position, range }
-    }
-}
+pub mod constrainer;
+pub mod slider;
